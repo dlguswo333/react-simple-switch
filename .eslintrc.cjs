e97ab728376b5bbdbd490c97a39c4bf7dda95025
dlguswo333 <dlguswo002@gmail.com>
2022-12-24 16:50:34 +0900

    Initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['*.js', '*.cjs'],
+  rules: {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'space-infix-ops': 'error',
+    'keyword-spacing': 'error',
+    'eol-last': 'error',
+    'comma-dangle': ['error', {
+      'arrays': 'never',
+      'objects': 'only-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
+    'space-before-function-paren': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'arrow-spacing': 'error',
+    '@typescript-eslint/type-annotation-spacing': ['error', {before: false, after: true}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  }
+};
