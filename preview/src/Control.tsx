f17847008fe754eb0dd2cc0467f947ea0f227e29
dlguswo333 <dlguswo002@gmail.com>
2023-01-23 12:54:54 +0900

    Add background color props in preview

--- preview/src/Control.tsx
+++ preview/src/Control.tsx
@@ -1,16 +1,23 @@
-import {FC, FormEventHandler, useCallback} from 'react';
+import {ChangeEventHandler, FC, FormEventHandler, useCallback} from 'react';
 import Switch from '@dlguswo333/react-simple-switch';
 
 type Props = {
   width: number;
   height: number;
   disabled: boolean;
+  onBackgroundColor?: string;
+  offBackgroundColor?: string;
   setWidth: (num: number) => void;
   setHeight: (num: number) => void;
   setDisabled: (val: boolean) => void;
+  setOnBackgroundColor: (val: string) => void;
+  setOffBackgroundColor: (val: string) => void;
 }
 
-const Control: FC<Props> = ({width, height, disabled, setWidth, setHeight, setDisabled}) => {
+const Control: FC<Props> = ({
+  width, height, disabled, onBackgroundColor, offBackgroundColor,
+  setWidth, setHeight, setDisabled, setOnBackgroundColor, setOffBackgroundColor
+}) => {
   const onWidthChange = useCallback<FormEventHandler<HTMLInputElement>>((e) => {
     const value = Number(e.currentTarget.value);
     if (isNaN(value)) {
@@ -31,6 +38,14 @@ const Control: FC<Props> = ({width, height, disabled, setWidth, setHeight, setDi
     setDisabled(value);
   }, [setDisabled]);
 
+  const onOnBackgroundColorChange = useCallback<ChangeEventHandler<HTMLInputElement>>((e) => {
+    setOnBackgroundColor(e.target.value);
+  }, [setOnBackgroundColor]);
+
+  const onOffBackgroundColorChange = useCallback<ChangeEventHandler<HTMLInputElement>>((e) => {
+    setOffBackgroundColor(e.target.value);
+  }, [setOffBackgroundColor]);
+
   return <section className='Control'>
     <div className='Panel'>
       <div>
@@ -50,10 +65,26 @@ const Control: FC<Props> = ({width, height, disabled, setWidth, setHeight, setDi
     </div>
     <div className='Panel'>
       <div>
-        <label>disabled: {disabled}</label>
+        <label>disabled: {disabled.toString()}</label>
       </div>
       <div>
         <Switch width={34} height={20} defaultValue={disabled} onChange={onDisabledChange} />
+      </div>
+    </div>
+    <div className='Panel'>
+      <div>
+        <label>onBackgroundColor: {onBackgroundColor}</label>
+      </div>
+      <div>
+        <input type='color' defaultValue={onBackgroundColor} onChange={onOnBackgroundColorChange} />
+      </div>
+    </div>
+    <div className='Panel'>
+      <div>
+        <label>offBackgroundColor: {offBackgroundColor}</label>
+      </div>
+      <div>
+        <input type='color' defaultValue={offBackgroundColor} onChange={onOffBackgroundColorChange} />
       </div>
     </div>
   </section>;
--- preview/src/Preview.tsx
+++ preview/src/Preview.tsx
@@ -5,19 +5,27 @@ import Control from './Control';
 const DEFAULT_SWITCH_PROPS = {
   WIDTH: 50,
   HEIGHT: 26,
+  ON_BACKGROUND_COLOR: undefined,
+  OFF_BACKGROUND_COLOR: undefined,
 };
 
 const Preview = () => {
   const [width, setWidth] = useState(DEFAULT_SWITCH_PROPS.WIDTH);
   const [height, setHeight] = useState(DEFAULT_SWITCH_PROPS.HEIGHT);
   const [disabled, setDisabled] = useState(false);
+  const [onBackgroundColor, setOnBackgroundColor] = useState<string|undefined>(DEFAULT_SWITCH_PROPS.ON_BACKGROUND_COLOR);
+  const [offBackgroundColor, setOffBackgroundColor] = useState<string|undefined>(DEFAULT_SWITCH_PROPS.OFF_BACKGROUND_COLOR);
+
   return <section className='Preview'>
     <div className='Switch-Holder'>
-      <Switch width={width} height={height} defaultValue={false} disabled={disabled} />
+      <Switch width={width} height={height} defaultValue={false} disabled={disabled}
+        onBackgroundColor={onBackgroundColor} offBackgroundColor={offBackgroundColor} />
     </div>
     <Control width={width} setWidth={setWidth}
       height={height} setHeight={setHeight}
-      disabled={disabled} setDisabled={setDisabled} />
+      disabled={disabled} setDisabled={setDisabled}
+      onBackgroundColor={onBackgroundColor} setOnBackgroundColor={setOnBackgroundColor}
+      offBackgroundColor={offBackgroundColor} setOffBackgroundColor={setOffBackgroundColor} />
   </section>;
 };
 
